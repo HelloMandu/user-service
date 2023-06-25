cf6f2eefd21d9bbd547e688af1821694c9fdc546
HelloMandu <tjdals6695@gmail.com>
2023-06-25 21:26:09 +0900

    ft: health-check logic 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormConfig } from './config/typeorm.config';
+import { HealthCheckController } from './health-check/health-check.controller';
 import authConfig from './config/auth.config';
 import emailConfig from './config/email.config';
 import validationSchema from './config/validation-schema';
+import { TerminusModule } from '@nestjs/terminus';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
@@ -17,8 +20,10 @@ import validationSchema from './config/validation-schema';
       validationSchema,
     }),
     TypeOrmModule.forRoot(typeormConfig),
+    TerminusModule,
+    HttpModule,
   ],
-  controllers: [],
+  controllers: [HealthCheckController],
   providers: [],
 })
 export class AppModule {}
---
+++ src/health-check/health-check.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
+
+@Controller('health-check')
+export class HealthCheckController {
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly http: HttpHealthIndicator,
+    private readonly db: TypeOrmHealthIndicator,
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([() => this.db.pingCheck('database')]);
+  }
+}
